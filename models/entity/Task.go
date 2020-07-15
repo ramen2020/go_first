e11e1eac0a5cc19c26cf4774d2cb8cb92ef944d9
ramen2020 <vfvsfv.dlk22@gmail.com>
2020-07-16 00:08:02 +0900

    add_task entity追加

---
+++ models/entity/Task.go
@@ -0,0 +1,9 @@
+package entity
+
+// Task モデル
+type Task struct {
+	ID    int    `gorm:"primary_key;not null"       json:"id"`
+	Name  string `gorm:"type:varchar(200);not null" json:"name"`
+	Memo  string `gorm:"type:varchar(400)"          json:"memo"`
+	State int    `gorm:"not null"                   json:"state"`
+}
